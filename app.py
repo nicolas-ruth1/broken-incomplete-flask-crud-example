9ac2d8a01ebfde85465bf654695b499dc76d5862
Nicolas Ruth <94563280+nicolas-ruth1@users.noreply.github.com>
2023-11-29 14:53:49 +0000

    commit3

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '35.246.57.140'
+app.config['MYSQL_HOST'] = '34.105.135.31'
 mysql.init_app(app)
 
 def execute_query(query):
